952faa236ea2da89e700e3deba27d0ee89b39406
sedtawut <73195479+sedtawut35085@users.noreply.github.com>
2025-09-29 14:47:13 +0700

    remove unused image processing code and retain main function for execution

--- main_mock.py
+++ main_mock.py
@@ -1,17 +1,6 @@
-from rembg import remove
-from PIL import Image
 
 def main():
-    input_path = 'dataset/rotten/x_20250601_223327.jpg'
-    output_path = 'temp_file/output_no_bg.png'
-
-    input_image = Image.open(input_path)
-
-    # ลบ background
-    output_image = remove(input_image)
-
-    # บันทึกไฟล์ PNG (transparent background)
-    output_image.save(output_path)
+    print('run main_mock.py')
 
 if __name__ == "__main__":
     main()
